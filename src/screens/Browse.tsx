d838f7d51e23af5c277d7e5b58943cf3c8bdab35
HongyeobKim <hngyb2585@gmail.com>
2021-11-12 22:38:49 +0900

    Update Browse

--- src/screens/Browse.tsx
+++ src/screens/Browse.tsx
@@ -1,31 +1,31 @@
-import React, { useCallback, useRef, useState } from "react";
+import React, { useCallback, useRef, useState } from 'react';
 import {
   Dimensions,
   StyleSheet,
   Text,
   TouchableOpacity,
   View,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import Icon from "react-native-vector-icons/Ionicons";
-import { NavigationHeader, TouchableView } from "../components";
-import Ranking from "../components/Ranking";
-import * as S from "./Styles";
-import { useNavigation } from "@react-navigation/core";
-import { ScrollView } from "react-native-gesture-handler";
-import { MyCrewCarousel } from "../components/MyCrewCarousel";
-import { EventCarousel } from "../components/EventCarousel";
-import { CrewNews } from "../components/CrewNews";
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { NavigationHeader, TouchableView } from '../components';
+import Ranking from '../components/Ranking';
+import * as S from './Styles';
+import { useNavigation } from '@react-navigation/core';
+import { ScrollView } from 'react-native-gesture-handler';
+import { MyCrewCarousel } from '../components/MyCrewCarousel';
+import { EventCarousel } from '../components/EventCarousel';
+import { CrewNews } from '../components/CrewNews';
 
-import { EventDummy } from "../components/Home/dummy";
+import { EventDummy } from '../components/Home/dummy';
 
-const deviceHeight = Dimensions.get("window").height;
+const deviceHeight = Dimensions.get('window').height;
 export default function Browse() {
   const navigation = useNavigation();
   const [loading, setLoading] = useState(false);
-  const [selectedRanking, setSelectedRanking] = useState("전체");
+  const [selectedRanking, setSelectedRanking] = useState('전체');
   const goSearch = useCallback(() => {
-    navigation.navigate("Search");
+    navigation.navigate('Search');
   }, []);
 
   return (
@@ -34,15 +34,15 @@ export default function Browse() {
         Right={() => (
           <View
             style={{
-              width: "100%",
+              width: '100%',
               borderBottomWidth: 1,
               borderColor: S.colors.secondary,
-              alignItems: "flex-end",
+              alignItems: 'flex-end',
             }}
           >
-            <TouchableView style={{ paddingHorizontal: "5%", marginBottom: 5 }}>
+            <TouchableView style={{ paddingHorizontal: '5%', marginBottom: 5 }}>
               <Icon
-                name={"search-outline"}
+                name={'search-outline'}
                 size={30}
                 color="black"
                 onPress={goSearch}
@@ -58,11 +58,11 @@ export default function Browse() {
         <View style={[styles.menuContainer]}>
           <View style={[styles.iconContainer]}>
             <TouchableOpacity style={[styles.iconWrapper]}>
-              <Icon name={"trophy"} size={40} style={[styles.icon]}></Icon>
+              <Icon name={'trophy'} size={40} style={[styles.icon]}></Icon>
               <Text style={[styles.menuText]}>랭킹</Text>
             </TouchableOpacity>
             <TouchableOpacity style={[styles.iconWrapper]}>
-              <Icon name={"people"} size={40} style={[styles.icon]}></Icon>
+              <Icon name={'people'} size={40} style={[styles.icon]}></Icon>
               <Text style={[styles.menuText]}>크루 모집</Text>
             </TouchableOpacity>
             <View style={[styles.iconWrapper]}>
@@ -93,7 +93,7 @@ export default function Browse() {
         </View>
         <View style={[styles.eventContainer]}>
           <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
+            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
           >
             <Text style={[styles.bigText, styles.contentTitle]}>이벤트</Text>
             <TouchableOpacity>
@@ -104,14 +104,14 @@ export default function Browse() {
         </View>
         <View style={[styles.newsContainer]}>
           <View
-            style={{ flexDirection: "row", justifyContent: "space-between" }}
+            style={{ flexDirection: 'row', justifyContent: 'space-between' }}
           >
             <Text style={[styles.bigText, styles.contentTitle]}>크루 소식</Text>
             <TouchableOpacity>
               <Text style={[styles.totalViewText]}>전체보기</Text>
             </TouchableOpacity>
           </View>
-          <View style={{ paddingHorizontal: "5%" }}>
+          <View style={{ paddingHorizontal: '5%' }}>
             <CrewNews dataArray={EventDummy} />
           </View>
         </View>
@@ -133,13 +133,13 @@ const styles = StyleSheet.create({
   },
   iconContainer: {
     flex: 1,
-    flexDirection: "row",
-    paddingHorizontal: "5%",
-    paddingVertical: "5%",
+    flexDirection: 'row',
+    paddingHorizontal: '5%',
+    paddingVertical: '5%',
   },
   iconWrapper: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
   },
   icon: {
     color: S.colors.primary,
@@ -164,18 +164,18 @@ const styles = StyleSheet.create({
   },
   contentTitle: {
     fontSize: 20,
-    paddingLeft: "5%",
-    paddingVertical: "3%",
-    textAlign: "left",
+    paddingLeft: '5%',
+    paddingVertical: '3%',
+    textAlign: 'left',
   },
   bigText: {
     fontFamily: S.fonts.bold,
   },
   totalViewText: {
     fontFamily: S.fonts.medium,
-    paddingRight: "5%",
-    paddingVertical: "3%",
-    textAlign: "left",
+    paddingRight: '5%',
+    paddingVertical: '3%',
+    textAlign: 'left',
     fontSize: 15,
     color: S.colors.secondary,
   },
@@ -186,14 +186,14 @@ const styles = StyleSheet.create({
   },
   categoryContainer: {
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
     marginHorizontal: 5,
     borderRadius: 20,
     borderWidth: 1,
   },
   category: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: 'center',
   },
   categoryText: {
     fontFamily: S.fonts.medium,
